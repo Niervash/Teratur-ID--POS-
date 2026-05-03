d69328ec7fe536cf1063732479449b3d03fcc69c
Niervash <julwalker38@gmail.com>
2026-05-03 22:31:48 +0800

    update

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-7a5e81cd'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.jrh62c1mqug"
+    "revision": "0.sumr51q2vg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
